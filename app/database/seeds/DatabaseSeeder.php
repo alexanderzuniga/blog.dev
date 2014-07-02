f59b88e33d1ee2bf9fd9059509652d5aeb1e6b53
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-02 14:14:55 -0500

    A development stage seeder to input sample data to fields on an extended level. Created admin user.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,22 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+
 	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $user = new User();
+        $user->email = 'admin@codeup.com';
+        $user->password = Hash::make('adminPass123!');
+        $user->save();
+    }
 
 }
