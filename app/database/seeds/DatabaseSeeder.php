b924bee49075e0b9fca3dcf9dbf6cd3c51f61a40
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-02 14:44:24 -0500

    updated for basic authentication. Can log in to blog although we cannot log out.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -17,16 +17,33 @@ class DatabaseSeeder extends Seeder {
 
 }
 
-class UserTableSeeder extends Seeder {
+// class UserTableSeeder extends Seeder {
+
+//     public function run()
+//     {
+//         DB::table('users')->delete();
+
+//         $user = new User();
+//         $user->email = 'admin@codeup.com';
+//         $user->password = Hash::make('adminPass123!');
+//         $user->save();
+//     }
+
+// }
+
+class PostsTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('users')->delete();
-
-        $user = new User();
-        $user->email = 'admin@codeup.com';
-        $user->password = Hash::make('adminPass123!');
-        $user->save();
+        DB::table('posts')->delete();
+        $i = 1;
+        do {
+	        $post = new Post();
+	        $post->title = 'Sample title: ' . $i;
+	        $post->body = 'Here is antoher story about ships and whales';
+	        $post->save();
+	        $i++;
+    	} while ($i < 15);
     }
 
 }
