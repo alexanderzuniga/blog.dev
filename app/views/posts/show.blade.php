777bbffddc7d7568f4e9f8130d489156a82d9d0d
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-07 10:20:14 -0500

    Added the user email of the author of the post. Adjusted time display.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h1>{{{ $post->title }}}	  </h1>
-<p> {{{ $post->created_at }}} </p>
+<p> {{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}} </p>
 <p> {{{ $post->body }}}		  </p>
 
 <a href="{{action('PostsController@edit', $post->id)}}" class="btn btn-success btn-sm" role="button">Edit</a>
