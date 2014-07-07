494516e3c974b2910ded243d4112284191143e4e
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-07 10:22:06 -0500

    Added Author col to display their email, and adjusted the time format

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -7,17 +7,26 @@
 <tr>
 	<th>Title</th>
 	<th>Creation date </th>
+	<th>Author</th>
 	<th>Actions</th>
 </tr>	
 @foreach ($posts as $post)
 <tr>
 	<td>{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}</td>
-	<td>{{{$post->created_at->setTimezone('America/Chicago')->format('F jS Y @ h:i A') }}}</td>
+	<td>{{{$post->created_at->format('l, F jS Y @ h:i A')}}}</td>
+	<td> {{{ $post->user->email }}} </td>
 	<td><a href="{{action('PostsController@edit', $post->id)}}" class="btn btn-success btn-sm">Edit</a></td>
 </tr>
 @endforeach	
 </table>
-	<a href="{{action('PostsController@create')}}" class="btn btn-primary btn-sm"> New Post </a>
+	<a href="{{action('PostsController@create') }}" class="btn btn-primary btn-sm"> New Post </a>
+	{{ Form::open(array('action'=> 'PostsController@index', 'class' => 'form-inline', 'method' => 'GET')) }}
+	<div class="form-group">
+		{{ Form::text('search', null, array('placeholder' => "Search Query", "class" => 'form-control col-lg-4')) }}
+	<button type="submit" class="btn btn-success">Search</button>
+	</div>
+<br>
+<div>
 	{{ $posts->links() }}
-
+</div>
 @stop
