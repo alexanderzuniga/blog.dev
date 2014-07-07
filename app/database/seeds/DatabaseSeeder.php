de368949ef1b5f26fce785732f943c8de8d886ea
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-07 10:15:38 -0500

    Production seeder to fill in blog posts and users. Hashing the passwords.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,39 +11,48 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		$this->call('UserTableSeeder');
-
+		// $this->call('UserTableSeeder');
+		$this->call('PostsTableSeeder');
+		
 	}
 
 }
 
-// class UserTableSeeder extends Seeder {
+class UserTableSeeder extends Seeder {
 
-//     public function run()
-//     {
-//         DB::table('users')->delete();
+    public function run()
+    {
+        DB::table('users')->delete();
 
-//         $user = new User();
-//         $user->email = 'admin@codeup.com';
-//         $user->password = Hash::make('adminPass123!');
-//         $user->save();
-//     }
+        $user = new User();
+        $user->email = 'admin@codeup.com';
+        $user->password = Hash::make('adminPass123!');
+        $user->save();
 
-// }
+        $user2 = new User();
+        $user2->email = 'admin02@codeup.com';
+        $user2->password = Hash::make('adminPass123!');
+        $user2->save();
+    }
+
+}
 
 class PostsTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('posts')->delete();
+
+        DB::table('posts');
+        // $user = User::first();
         $i = 1;
         do {
 	        $post = new Post();
+	       	$post->user_id = rand(1,2);
 	        $post->title = 'Sample title: ' . $i;
 	        $post->body = 'Here is antoher story about ships and whales';
 	        $post->save();
 	        $i++;
-    	} while ($i < 15);
+    	} while ($i < 10);
     }
 
 }
