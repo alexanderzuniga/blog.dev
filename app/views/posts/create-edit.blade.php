4847a74cc281137611d6dcbb84aacaa55903a45b
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-08 10:30:10 -0500

    Added option to load image to post.

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-	@if (isset($post))
+	@if (isset($post) && Auth::check())
 		<h1>Edit Post</h1>
-		{{ Form::model($post, array('action'=>array('PostsController@update', $post->id), 'method'=>'PUT')) }}
+		{{ Form::model($post, array('action'=>array('PostsController@update', $post->id), 'method'=>'PUT', 'files' => true)) }}
 	@else
 		<h1>Create a blog post! </h1>
-		{{ Form::open(array('action'=>array('PostsController@store'))) }}
+		{{ Form::open(array('action'=>array('PostsController@store'), 'files' => true)) }}
 	@endif	
 
 	@if ($errors->has('title'))
@@ -15,13 +15,16 @@
 		{{ $errors->first('body', '<span class="help-block">:message</span>') }}
 	@endif
 	<div>
-	
-	{{ Form::label('title', 'Title')}}<br>
-	{{ Form::text('title')}}
+	{{ Form::label('title', 'Title') }}<br>
+	{{ Form::text('title') }}
 	</div>
 	<div>
-	{{ Form::label('body', 'Body')}}<br>
-	{{ Form::textarea('body')}} 
+	{{ Form::label('image', 'Image') }} <br>
+	{{ Form::file('image') }}
+	</div>
+	<div>
+	{{ Form::label('body', 'Body') }}<br>
+	{{ Form::textarea('body') }} 
 	{{ Form::submit('Enter') }}
 	{{ Form::close() }}
 	</div>
