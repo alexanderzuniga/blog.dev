98804cef38079e0b7de5fa1c82b6ff814aefc570
Alex Zuniga <guest@Alexs-MBA.local>
2014-07-08 10:33:32 -0500

    Shows the uploaded image. Also filters if user is logged in. If so, buttons to edit and delete the post appear.

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,16 +2,25 @@
 
 @section('content')
 
-<h1>{{{ $post->title }}}	  </h1>
-<p> {{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}} </p>
-<p> {{{ $post->body }}}		  </p>
+<h1>{{{ $post->title }}}	   </h1>
+<p> {{{ $post->created_at->format('l, F jS Y @ h:i A') }}} </p>
+<p> {{{ $post->user->email }}} </p>
+@if ($post->img_path)
+	<img src="{{{ $post->img_path }}}" class="img-responsive"> 
 
+@endif
+<p> {{{ $post->body }}}	 </p>
+
+@if (Auth::check())
 <a href="{{action('PostsController@edit', $post->id)}}" class="btn btn-success btn-sm" role="button">Edit</a>
-<a href="{{action('PostsController@index')}}" class="btn btn-default btn-sm" role="button"> Back </a> 
-
+<div class="form-group">
 {{ Form::open(array('action'=>array('PostsController@destroy', $post->id), 'method'=>'DELETE')) }}
-	{{ Form::submit('Delete') }}
+<button type="submit" class="btn btn-danger">Delete</button>
 {{ Form:: close() }}
-
+@else
+@endif
+<p>
+<a href="{{action('PostsController@index')}}" class="btn btn-default btn-sm" role="button"> Back </a> 
+</p>
 </div>
 @stop
